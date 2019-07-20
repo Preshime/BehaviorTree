26c2db664c7f530e73d7dcc305aeb37e7c25732b
preshime <preshime@outlook.com>
2019-07-20 21:12:31 +0900

    测试使用

---
+++ BehaviorTreeTest/Assets/Scripts/NodeTest/SingleTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTest : SingleNode
+{
+    public SingleTest(ControllerNode Parent) : base(Parent)
+    {
+    }
+
+    public override bool CheckSelf()
+    {
+        int i;
+        if (Model.TryGetValue<int>("123", out i))
+        {
+            return i < 5;
+        }
+        return false;
+    }
+
+}
